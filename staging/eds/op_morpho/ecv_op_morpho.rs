03f656a5da529893e16d03b77bf22c15d5be409f
Emmanuel DUMAS <emmanuel.dumas@free.fr>
2020-11-14 18:00:09 +0100

    Creation

---
+++ staging/eds/op_morpho/ecv_op_morpho.rs
@@ -0,0 +1,12 @@
+// ecv_op_morpho.rs
+// ----------------------------------------------------------------------------
+// My first try of image processing
+
+
+fn ECV_opMorpho(width: i32)
+{
+    println!("The value of width is: {}", width);
+}
+
+
+// end of file
